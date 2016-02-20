776577be2915c0bdb883d8363c9e91287b46e8d8
Prateek Srivastava <f2prateek@gmail.com>
2016-02-20 07:05:20 -0700

    backoff policy

---
+++ backoff/backoff.go
@@ -0,0 +1,50 @@
+package backoff
+
+import (
+	"github.com/f2prateek/hickson"
+	"github.com/segmentio/backo-go"
+)
+
+var Default = New(backo.DefaultBacko())
+
+func New(b *backo.Backo) hickson.RetryPolicyFactory {
+	return &factory{b}
+}
+
+type factory struct {
+	b *backo.Backo
+}
+
+func (f *factory) NewRetryPolicy() hickson.RetryPolicy {
+	ticker := f.b.NewTicker()
+	return &retryPolicy{ticker, make(chan struct{})}
+}
+
+type retryPolicy struct {
+	ticker *backo.Ticker
+	done   chan struct{}
+}
+
+func (p *retryPolicy) Retry() <-chan struct{} {
+	out := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-p.ticker.C:
+				out <- struct{}{}
+			case <-p.done:
+				p.ticker.Stop()
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func (p *retryPolicy) Cancel() <-chan struct{} {
+	return make(chan struct{})
+}
+
+func (p *retryPolicy) Close() {
+	p.done <- struct{}{}
+}
