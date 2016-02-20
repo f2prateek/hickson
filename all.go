18138f4142837dfdad7406245e684a32a60b25ea
Prateek Srivastava <f2prateek@gmail.com>
2016-02-20 07:11:40 -0700

    add RetryAll

---
+++ all.go
@@ -0,0 +1,79 @@
+package hickson
+
+import "sync"
+
+func RetryAll(factories ...RetryPolicyFactory) RetryPolicyFactory {
+	return &retryAllPolicyFactory{factories}
+}
+
+type retryAllPolicyFactory struct {
+	factories []RetryPolicyFactory
+}
+
+func (f *retryAllPolicyFactory) NewRetryPolicy() RetryPolicy {
+	policies := make([]RetryPolicy, len(f.factories))
+	for _, policyFactory := range f.factories {
+		policies = append(policies, policyFactory.NewRetryPolicy())
+	}
+	return &retryAllPolicy{policies}
+}
+
+type retryAllPolicy struct {
+	policies []RetryPolicy
+}
+
+func (all *retryAllPolicy) Retry() <-chan struct{} {
+	// Retry emits on out once *all* of the policies emit on their retry channel.
+	out := make(chan struct{})
+
+	go func() {
+		for {
+			var wg sync.WaitGroup
+			for _, policy := range all.policies {
+				wg.Add(1)
+				go func(policy RetryPolicy) {
+					defer wg.Done()
+					<-policy.Retry()
+				}(policy)
+			}
+			wg.Wait()
+			out <- struct{}{}
+		}
+	}()
+
+	return out
+}
+
+func (p *retryAllPolicy) Cancel() <-chan struct{} {
+	// Cancel emits on out once *any* of the policies emit on their cancel channel.
+	out := make(chan struct{})
+
+	go func() {
+		for {
+			merged := make(chan struct{}, len(p.policies))
+			done := make(chan struct{})
+			defer close(done)
+
+			for _, policy := range p.policies {
+				go func(policy RetryPolicy) {
+					select {
+					case <-policy.Cancel():
+						merged <- struct{}{}
+					case <-done:
+						return
+					}
+				}(policy)
+			}
+
+			out <- <-merged
+		}
+	}()
+
+	return out
+}
+
+func (p *retryAllPolicy) Close() {
+	for _, policy := range p.policies {
+		policy.Close()
+	}
+}
